fb76f4bfb46b174916f0d0e240406ebd8a462a6c
한지연 <165872533+jIYEON14@users.noreply.github.com>
2024-05-06 22:21:23 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 356 ms, Memory: 18356 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int o = 1; o <= i; o++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
