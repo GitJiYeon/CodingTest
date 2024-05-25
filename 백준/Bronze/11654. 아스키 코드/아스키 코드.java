cb9b96eec05e7549dd46a5ca16f9240ac5c5d0e9
한지연 <165872533+jIYEON14@users.noreply.github.com>
2024-05-25 22:30:23 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char inputChar = scanner.next().charAt(0);
+        int asciiValue = (int) inputChar;
+        System.out.println(asciiValue);
+        scanner.close();
+    }
+}
