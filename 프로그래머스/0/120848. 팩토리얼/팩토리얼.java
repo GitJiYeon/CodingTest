1fee8253149f2d1110e02907fc663a6e681fc8c6
한지연 <165872533+jIYEON14@users.noreply.github.com>
2024-06-03 08:11:14 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int i = 1;
+        int f = 1;
+        
+        while (f <= n) {
+            i++;
+            f *= i;
+        }
+        
+        return i - 1;
+    }
+}
