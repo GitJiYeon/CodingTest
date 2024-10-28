0ccc9eb43ff733c710d96f556a1b0eb2c70a9a6d
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2024-10-28 21:55:09 +0900

    [level 0] Title: 배열 만들기 1, Time: 5.45 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/README.md
+++ 프로그래머스/0/181901. 배열 만들기 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 1 - 181901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181901?language=csharp) 
 
 ### 성능 요약
 
-메모리: 84.1 MB, 시간: 8.96 ms
+메모리: 31.3 MB, 시간: 5.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 14일 21:18:44
+2024년 10월 28일 21:55:05
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution {
+    public int[] solution(int n, int k) {
+        int count = k;
+        int acount = 0;
+        for(int i = 1; i <= n; i++){
+            if(i % k == 0){
+                acount++;
+            }
+        }
+        int[] answer = new int[acount];
+        
+        acount = 0;
+        for(int i = 1; i <= n; i++){
+            if(i % k == 0){
+                answer[acount] = i;
+                acount++;
+            }
+        }
+        return answer;
+    }
+}
