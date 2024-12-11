3429f56a207fb7cbcf759b426b12c5971edd0520
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2024-12-11 22:30:23 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.50 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(string number) {
+        int sum = 0;
+
+        foreach (char c in number)
+        {
+            sum += c - '0';
+        }
+
+        return sum % 9;
+    }
+}
