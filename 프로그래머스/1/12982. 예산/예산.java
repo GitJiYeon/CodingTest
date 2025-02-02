64c7301dc493e702557dc9cb2bfe8a2abcb0a94c
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2025-02-02 23:27:11 +0900

    [level 1] Title: 예산, Time: 0.69 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int count = 0;
+        int sum = 0;
+        
+        for (int cost : d) {
+            if (sum + cost > budget) {
+                break;
+            }
+            sum += cost;
+            count++;
+        }
+        
+        return count;
+    }
+}
