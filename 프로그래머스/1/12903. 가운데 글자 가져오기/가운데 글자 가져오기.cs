87dca365bd31873bdaf76c0577face923da8ff39
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2025-03-24 08:20:32 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.44 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
@@ -1,15 +1,9 @@
 public class Solution {
     public string solution(string s) {
-       if(s.Length % 2 == 0){
-            char[] ch = new char[2];
-                ch[0] = s[s.Length/2-1];
-                ch[1] = s[s.Length/2];
-            return new string(ch);
-        }
-        else{
-            char[] ch = new char[1];
-            ch[0] = s[s.Length/2];
-            return new string(ch);
+        if (s.Length % 2 == 0) {
+            return s[(s.Length / 2) - 1].ToString() + s[s.Length / 2].ToString();
+        } else {
+            return s[s.Length / 2].ToString();
         }
     }
 }
