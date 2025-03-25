2effbf5768f4082c01b15aa1c60fcaf8bd4e7836
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2025-03-25 11:29:27 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.66 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,47 +1,17 @@
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] arr) {
-        int mini = Integer.MAX_VALUE;
-        int[] answer = {-1};
-        if(arr.length == 1){
-            return answer;
+        int min = Arrays.stream(arr).min().getAsInt();
+        if(arr.length < 2) {
+            return new int[]{-1};
         }
+        int[] answer = new int[arr.length-1];
+        int count = 0;
         for(int i = 0; i < arr.length; i++){
-            if(mini > arr[i]){
-                mini = arr[i];
-            }
-            
-        }
-        
-        for(int i = 0; i < arr.length; i++){
-            if(mini == arr[i]){
-                arr[i] = Integer.MAX_VALUE;
+            if(arr[i] != min){
+                answer[count++] = arr[i];
             }
         }
-        int [] arr2 = new int[arr.length-1];
-        
-      /*  for(int i = 0, j = 0; i < arr.length && j < arr2.length; i++, j++){
-            if(arr[i] != Integer.MAX_VALUE){
-                arr2[j] = arr[i];
-            }
-            else {
-                j++;
-            }
-        }*/
-        
-        int arrN = 0;
-        int arr2N = 0;
-       while(arr2N < arr2.length){
-            if(arr[arrN] != Integer.MAX_VALUE){
-                arr2[arr2N] = arr[arrN];
-                arr2N++;
-                arrN++;
-            }
-            else {
-                arrN++;
-            }
-        }
-        
-        answer = arr2;
         return answer;
     }
 }
