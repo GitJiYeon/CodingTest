25a7f27f4ad333b49efc61d854790edf55e6c663
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2025-05-08 14:57:12 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,9 +1,6 @@
 def solution(my_string, s, e):
-    answer = ''
-    for i in range(s):
-        answer += my_string[i]
-    for i in range(e, s-1,-1):
-        answer += my_string[i]
-    for i in range(e+1, len(my_string)):
-        answer += my_string[i]
-    return answer
+    result = []
+    result.append(my_string[:s])
+    result.append(my_string[s:e+1][::-1])
+    result.append(my_string[e+1:])
+    return ''.join(result)
