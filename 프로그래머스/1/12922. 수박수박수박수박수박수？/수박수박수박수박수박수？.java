14f19b8f29654cb21b59141ff3309fe2683e71ac
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2025-05-16 12:14:00 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 46.17 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,15 +1,10 @@
 class Solution {
     public String solution(int n) {
-        char [] ch = new char[n];
-        for(int i = 0; i < n; i+=2){
-            ch[i] = '수';
-            
+        String answer = "";
+        for(int i = 0; i< n; i++){
+            if(i % 2 == 0) answer += "수";
+            else answer += "박";
         }
-        for(int i = 1; i < n; i+=2){
-            ch[i] = '박';
-            
-        }
-        String answer = new String(ch);
         return answer;
     }
 }
