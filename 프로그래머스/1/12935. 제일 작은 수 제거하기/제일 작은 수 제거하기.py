f4a5a6df5858470555c1669df03bfdb3be95e646
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2025-05-24 21:11:39 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 4.87 ms, Memory: 15 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,11 +1,15 @@
 def solution(arr):
     answer = []
+    min_value = min(arr)
+    """
+    아니 파이썬에는 min() 함수가 있다고요?
     min = arr[0]
     for i in range(len(arr)):
         if(min > arr[i]):
             min = arr[i]
+    """
     for i in range(len(arr)):
-        if(arr[i] != min):
+        if(arr[i] != min_value):
             answer.append(arr[i])
     if len(answer) != 0:
         return answer
