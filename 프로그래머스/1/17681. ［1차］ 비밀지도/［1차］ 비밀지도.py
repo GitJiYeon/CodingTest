b9f0269e0945c84ef46cc567e1476e2fe3780a43
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2025-07-17 09:07:20 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,14 @@
+def solution(n, arr1, arr2):
+    answer = []
+    for n1, n2 in zip(arr1, arr2):
+        tempStr = ''
+        num1 = format(n1, 'b').zfill(n) #  .zfill(n) n자리를 0으로 채움
+        num2 = format(n2, 'b').zfill(n)
+        for i, j in zip(num1, num2):
+            if i == '0' and j == '0':
+                tempStr+=' '
+            else:
+                tempStr+='#'
+        answer.append(tempStr)
+        
+    return answer
