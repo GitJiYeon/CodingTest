cae153179cc850430b7dbb7483e73f6f0918b948
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2025-07-17 09:14:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.05 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 0.04 ms
+메모리: 9.11 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 17일 09:07:17
+2025년 07월 17일 09:14:54
 
 ### 문제 설명
 
--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -4,11 +4,16 @@ def solution(n, arr1, arr2):
         tempStr = ''
         num1 = format(n1, 'b').zfill(n) #  .zfill(n) n자리를 0으로 채움
         num2 = format(n2, 'b').zfill(n)
+        
+        #아래 4줄을 한번에 작성
+        tempStr = ''.join('#' if i != '0' or j != '0' else ' ' for i, j in zip(num1, num2))
+        """
         for i, j in zip(num1, num2):
             if i == '0' and j == '0':
                 tempStr+=' '
             else:
                 tempStr+='#'
+        """
         answer.append(tempStr)
         
     return answer
