5e160f04bdc96e718ca5031e6e43c3381e6a088d
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2025-08-19 23:19:19 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,10 @@
+function solution(common) {
+    if (common[1] - common[0] === common[2] - common[1]) {
+        let d = common[1] - common[0]; 
+        return common[common.length - 1] + d;
+    }
+    else {
+        let r = common[1] / common[0]; 
+        return common[common.length - 1] * r;
+    }
+}
