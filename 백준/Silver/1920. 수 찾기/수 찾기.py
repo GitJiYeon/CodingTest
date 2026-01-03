a608b2367a6d23efdbfb99062e615fe6183a8dd1
한지연 <165872533+GitJiYeon@users.noreply.github.com>
2026-01-03 11:10:12 +0900

    [Silver IV] Title: 수 찾기, Time: 424 ms, Memory: 48992 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,19 +1,17 @@
 N = int(input())
-A = list(map(int, input().split()))
+A = sorted( list(map(int, input().split())) )
 M = int(input())
 B = list(map(int, input().split()))
 
-A.sort()
-#있는지 확인
 def include_num(num):
     left = 0
-    right = N-1
+    right = N - 1
     while left <= right:
-        mid = (left+right)//2
+        mid = (left+right) // 2
         if(A[mid] > num):
-            right = mid -1
+            right = mid - 1
         elif(A[mid] < num):
-            left = mid +1
+            left = mid + 1
         else:
             return 1
     return 0
